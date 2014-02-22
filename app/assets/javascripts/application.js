c2d963b9d5da4e15ff819929e3a1a48f4eb7123b
nikita <nikitaa_singh@yahoo.co.in>
2014-02-22 13:31:27 +0530

    adding texttocolor and white or black javascript, not using gem for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,15 +5,14 @@
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
+// the compiled file.
 //
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
 //
+
 //= require jquery
 //= require jquery_ujs
-//= require foundation
-//= require turbolinks
 //= require jquery.ui.core
 //= require jquery.ui.widget
 //= require jquery.ui.position
@@ -21,7 +20,6 @@
 //= require jquery.ui.autocomplete
 //= require underscore-1.4.3
 //= require backbone-0.9.10
-
 //= require jquery.ui.datepicker
 
 //= require visualsearch/js/visualsearch
@@ -36,4 +34,43 @@
 //= require visualsearch/js/utils/search_parser
 //= require visualsearch/js/utils/inflector
 //= require visualsearch/js/templates/templates
+
 //= require_tree .
+
+
+var TextToColour = function(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var colour = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
+
+
+
+function idealTextColor(bgColor) {
+
+   var nThreshold = 105;
+   var components = getRGBComponents(bgColor);
+   var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+
+   return ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff";   
+}
+
+function getRGBComponents(color) {       
+
+    var r = color.substring(1, 3);
+    var g = color.substring(3, 5);
+    var b = color.substring(5, 7);
+
+    return {
+       R: parseInt(r, 16),
+       G: parseInt(g, 16),
+       B: parseInt(b, 16)
+    };
+}
